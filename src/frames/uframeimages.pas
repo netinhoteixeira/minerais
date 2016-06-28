6212bcbf51928d5982ae8ad448d052456fce9b5f
elidioxg <elidioxg@gmail.com>
2016-06-28 12:51:30 -0300

    Implementing uframeimages

--- src/frames/uframeimages.pas
+++ src/frames/uframeimages.pas
@@ -5,13 +5,15 @@ unit uframeimages;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls;
+  Classes, SysUtils, FileUtil, Forms, Controls, ExtCtrls, udatamodule,
+  unitBlobFields;
 
 type
 
   { TFrameImage }
 
   TFrameImage = class(TFrame)
+    ScrollBox1: TScrollBox;
   private
     { private declarations }
   public
@@ -22,8 +24,20 @@ type
     { public declarations }
   end;
 
+
   const
     Lista_Formatos: string = 'All Files| *.jpg; *.jpeg;';
+    ImagesLeft: integer = 6;
+  Image1Top: integer = 8;
+  SeparadorImagens: integer = 140;
+  ImagesWidth: integer = 140;
+  ImagesHeight: integer = 120;
+
+  ImagesMargin: Integer = 8;
+  ImagesSize: Integer = 120;
+
+  var
+    Images:Array[0..20] of TImage;
 
 implementation
 
@@ -42,8 +56,26 @@ begin
 end;
 
 procedure TFrameImage.RefreshImages(MineralName: String; Count:Integer);
+var I:Integer; SQL:String;
 begin
-
+  SQL:='SELECT '+FieldName+' FROM '+Dados.Table5+' WHERE '+FieldName+'="'+MineralName+'"';
+  for I:=0 to Count-1 do
+  begin
+    Images[I] :=TImage.Create(nil);
+    with Images[I] do
+    begin
+      Parent:=ScrollBox1;
+      Left:=ImagesLeft;
+      Top:=Trunc(Image1Top + (I*(Image1Top+ImagesHeight)));
+      Width:=ImagesWidth;
+      Height:=ImagesHeight;
+      Proportional:=True;
+      Center:=True;
+      //AntialiasingMode := amOn;
+      //OnClick := @Image1Click;
+      Picture.Graphic:=  SelectImage(MineralName, I);
+    end;
+  end;
 
 end;
 
