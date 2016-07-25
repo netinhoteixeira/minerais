d2d69e499de69ce02806d2d585862144c4cf8367
elidioxg <elidioxg@gmail.com>
2016-07-25 02:11:30 -0300

    Form for removing images

---
+++ src/forms/uformrmimage.pas
@@ -0,0 +1,62 @@
+unit uformrmimage;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls, ActnList, uframeimages, udatamodule, unitblobfields;
+
+type
+
+  { TFormRmImage }
+
+  TFormRmImage = class(TForm)
+    ActionListClick: TAction;
+    ActionList1: TActionList;
+    ListBox1: TListBox;
+    Panel1: TPanel;
+    procedure ActionListClickExecute(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+    Frame:TFrameImage;
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  FormRmImage: TFormRmImage;
+
+implementation
+
+{$R *.lfm}
+
+{ TFormRmImage }
+
+procedure TFormRmImage.FormCreate(Sender: TObject);
+begin
+   Frame:=TFrameImage.Create(nil);
+   with Frame do
+  begin
+    Parent:=FormRmImage;
+    Align:=alClient;
+  end;
+  ListBox1.Items.AddStrings(Dados.ReturnAllMinerals(ASCII));
+end;
+
+procedure TFormRmImage.ActionListClickExecute(Sender: TObject);
+begin
+   Frame.RefreshLarge(ListBox1.GetSelectedText,
+     MineralImagesCount(ListBox1.GetSelectedText));
+end;
+
+procedure TFormRmImage.FormDestroy(Sender: TObject);
+begin
+   Frame.Free;
+end;
+
+end.
+
